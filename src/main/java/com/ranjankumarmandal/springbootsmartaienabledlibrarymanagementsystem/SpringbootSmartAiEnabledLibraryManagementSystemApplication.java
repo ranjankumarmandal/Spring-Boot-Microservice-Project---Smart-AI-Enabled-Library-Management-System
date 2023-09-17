00f6603377e71498a6355f1eb0bfbaf08e5592af
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2023-09-17 17:45:55 +0530

    Spring Boot: Smart AI Library Management System Backend Application Initialization

---
+++ src/main/java/com/ranjankumarmandal/springbootsmartaienabledlibrarymanagementsystem/SpringbootSmartAiEnabledLibraryManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.ranjankumarmandal.springbootsmartaienabledlibrarymanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSmartAiEnabledLibraryManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSmartAiEnabledLibraryManagementSystemApplication.class, args);
+	}
+
+}
