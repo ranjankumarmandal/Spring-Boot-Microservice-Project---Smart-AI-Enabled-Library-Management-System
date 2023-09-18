04786fd2d72e73ea3657e8fb1a9da825591154bf
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2023-09-18 14:06:00 +0530

    Spring Boot: completed implementation of entity class Book

--- src/main/java/com/ranjankumarmandal/springbootsmartaienabledlibrarymanagementsystem/model/Book.java
+++ src/main/java/com/ranjankumarmandal/springbootsmartaienabledlibrarymanagementsystem/model/Book.java
@@ -1,4 +1,36 @@
 package com.ranjankumarmandal.springbootsmartaienabledlibrarymanagementsystem.model;
 
+import lombok.*;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
 public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 30)
+    private String name;
+
+    private int cost;
+
+    @JoinColumn
+    @ManyToOne
+    private Author author;
+
+    private String bookNo;
+
+    @Enumerated(value = EnumType.STRING)
+    private BookType type;
+
+    @JoinColumn
+    @ManyToOne
+    private Student student;
 }
