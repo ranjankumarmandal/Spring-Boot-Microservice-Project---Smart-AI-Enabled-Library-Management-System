06c5223dc4863299f155016136ace4bb93df51d6
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2023-09-18 13:50:46 +0530

    String Boot: started working for entity Author

--- src/main/java/com/ranjankumarmandal/springbootsmartaienabledlibrarymanagementsystem/model/Author.java
+++ src/main/java/com/ranjankumarmandal/springbootsmartaienabledlibrarymanagementsystem/model/Author.java
@@ -1,4 +1,24 @@
 package com.ranjankumarmandal.springbootsmartaienabledlibrarymanagementsystem.model;
 
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
+@Entity
 public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    
 }
