b1fc3565ccf4e396ace46ed0302d62f29c4be433
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2023-09-18 14:52:35 +0530

    Spring Boot: completed implementaion of Service level class BookService

--- src/main/java/com/ranjankumarmandal/springbootsmartaienabledlibrarymanagementsystem/service/BookService.java
+++ src/main/java/com/ranjankumarmandal/springbootsmartaienabledlibrarymanagementsystem/service/BookService.java
@@ -1,7 +1,29 @@
 package com.ranjankumarmandal.springbootsmartaienabledlibrarymanagementsystem.service;
 
+import com.ranjankumarmandal.springbootsmartaienabledlibrarymanagementsystem.model.Author;
+import com.ranjankumarmandal.springbootsmartaienabledlibrarymanagementsystem.model.Book;
+import com.ranjankumarmandal.springbootsmartaienabledlibrarymanagementsystem.repository.AuthorRepository;
+import com.ranjankumarmandal.springbootsmartaienabledlibrarymanagementsystem.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BookService {
+
+    @Autowired
+    BookRepository bookRepository;
+
+    @Autowired
+    AuthorRepository authorRepository;
+
+    public void create(CreateBookRequest createBookRequest) {
+        Book book = createBookRequest.to();
+        Author authorFromDB = authorRepository.findByEmail(book.getAuthor().getEmail());
+
+        if(authorFromDB == null) {
+            authorFromDB = authorRepository.save(book.getAuthor());
+        }
+
+        bookRepository.save(book);
+    }
 }
