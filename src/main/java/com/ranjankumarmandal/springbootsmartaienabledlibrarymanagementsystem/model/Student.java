a93c2ac3926556408fd9e08056cb94a04769d87d
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2023-09-18 14:12:36 +0530

    Spring Boot: completed implementation of entity class Student

--- src/main/java/com/ranjankumarmandal/springbootsmartaienabledlibrarymanagementsystem/model/Student.java
+++ src/main/java/com/ranjankumarmandal/springbootsmartaienabledlibrarymanagementsystem/model/Student.java
@@ -1,4 +1,46 @@
 package com.ranjankumarmandal.springbootsmartaienabledlibrarymanagementsystem.model;
 
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
 public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 30)
+    private String name;
+
+    @Column(length = 30)
+    private String address;
+
+    @Column(nullable = false, unique = true)
+    private String contact;
+
+    @Column(length = 30, unique = true)
+    private String email;
+
+    @CreationTimestamp
+    private Date createdOn;
+
+    @UpdateTimestamp
+    private Date updatedOn;
+
+    @OneToMany
+    private List<Book> bookList;
+
+    @Enumerated
+    private StudentType studentType;
 }
