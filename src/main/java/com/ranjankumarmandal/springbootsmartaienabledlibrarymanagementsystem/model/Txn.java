0656f74efa7825ed4902c3d690669604a853a912
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2023-09-18 14:16:49 +0530

    Spring Boot: completed implementation for entity class Txn

--- src/main/java/com/ranjankumarmandal/springbootsmartaienabledlibrarymanagementsystem/model/Txn.java
+++ src/main/java/com/ranjankumarmandal/springbootsmartaienabledlibrarymanagementsystem/model/Txn.java
@@ -1,4 +1,40 @@
 package com.ranjankumarmandal.springbootsmartaienabledlibrarymanagementsystem.model;
 
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
 public class Txn {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @JoinColumn
+    @ManyToOne
+    private Student student;
+
+    @JoinColumn
+    @ManyToOne
+    private Book book;
+
+    private int paidCost;
+
+    @Enumerated(value = EnumType.STRING)
+    private TxnStatus txnStatus;
+
+    @CreationTimestamp
+    private Date createdOn;
+
+    @UpdateTimestamp
+    private Date updatedOn;
 }
