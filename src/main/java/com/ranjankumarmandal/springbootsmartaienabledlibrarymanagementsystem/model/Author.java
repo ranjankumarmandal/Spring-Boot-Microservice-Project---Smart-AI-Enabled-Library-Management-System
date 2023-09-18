58dc833381a9570c476688aa3b41a1995e521321
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2023-09-18 13:56:56 +0530

    Spring Boot: completed implementaion of entity class Author

--- src/main/java/com/ranjankumarmandal/springbootsmartaienabledlibrarymanagementsystem/model/Author.java
+++ src/main/java/com/ranjankumarmandal/springbootsmartaienabledlibrarymanagementsystem/model/Author.java
@@ -1,11 +1,12 @@
 package com.ranjankumarmandal.springbootsmartaienabledlibrarymanagementsystem.model;
 
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
 
 @Getter
 @Setter
@@ -20,5 +21,18 @@ public class Author {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    
+    @Column(length = 30)
+    private String name;
+
+    @Column(length = 30, unique = true, nullable = false)
+    private String email;
+
+    @OneToMany(mappedBy = "author")
+    private List<Book> bookList;
+
+    @CreationTimestamp
+    private Date createdOn;
+
+    @UpdateTimestamp
+    private Date updateOn;
 }
