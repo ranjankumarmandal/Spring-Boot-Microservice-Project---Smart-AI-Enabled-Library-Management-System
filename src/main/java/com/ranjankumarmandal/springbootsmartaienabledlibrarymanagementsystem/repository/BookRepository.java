02cb3fa71c1180e44db93bdf9d75bbb0c501225a
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2023-09-18 14:39:57 +0530

    Spring Boot: Completed implementaion for class Book Repository

---
+++ src/main/java/com/ranjankumarmandal/springbootsmartaienabledlibrarymanagementsystem/repository/BookRepository.java
@@ -0,0 +1,6 @@
+package com.ranjankumarmandal.springbootsmartaienabledlibrarymanagementsystem.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository  extends JpaRepository {
+}
